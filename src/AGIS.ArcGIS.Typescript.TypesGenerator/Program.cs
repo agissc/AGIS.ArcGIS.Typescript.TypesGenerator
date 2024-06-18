df62b4a84559c58b5822ea39c5c5c571c50a08cb
Balz Steinemann <balz.steinemann@gmail.com>
2024-06-18 09:59:08 +0200

    Init repository

---
+++ src/AGIS.ArcGIS.Typescript.TypesGenerator/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ArcGIS.Core.CIM;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using NJsonSchema;
+
+
+var contractResolver = new DefaultContractResolver
+{
+    NamingStrategy = new CamelCaseNamingStrategy()
+};
+
+var jsonSettings = new NJsonSchema.NewtonsoftJson.Generation.NewtonsoftJsonSchemaGeneratorSettings
+{
+    SerializerSettings = new JsonSerializerSettings
+    {
+        ContractResolver = contractResolver
+    }
+};
+
+
+var schema = JsonSchema.FromType<CIMLayerDocument>(jsonSettings);
+var schemaJson = schema.ToJson();
+
+File.WriteAllText("layer-document.json", schemaJson);
